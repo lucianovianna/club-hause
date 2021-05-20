45a2d7ef5f17b18d1f5f94fdf3c6eb894d4b9986
lucianovianna <luciano11v11@gmail.com>
2021-05-19 21:10:35 -0300

    criando roomsController && linkando o socket.io com os eventos e controllers

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,39 @@
+import RoomsController from "./controllers/roomsControllers.js";
 import SocketServer from "./util/socket.js";
+import Event from "events";
+import { constants } from "./util/constants.js";
 
 const port = process.env.PORT || 3000;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.start();
+
+const roomsController = new RoomsController();
+
+const namespaces = {
+  room: { controller: roomsController, eventEmitter: new Event() },
+};
+
+const routeConfig = Object.entries(namespaces).map(
+  ([namespace, { controller, eventEmitter }]) => {
+    const controllerEvents = controller.getEvents();
+    eventEmitter.on(
+      constants.event.USER_CONNECTED,
+      controller.onNewConnection.bind(controller)
+    );
+
+    return {
+      [namespace]: { events: controllerEvents, eventEmitter },
+    };
+  }
+);
+
+socketServer.attachEvents({ routeConfig });
+
+// namespaces.room.eventEmitter.on(
+//   "userConnected",
+//   namespaces.room.controller.onNewConnection.bind(namespaces.room.controller)
+// );
+
+// namespaces.room.eventEmitter.emit("userConnected", {id: "UIDTESTE"});
 
 console.log("Socket server is running at port", server.address().port);
